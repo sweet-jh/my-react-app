fc73f8d608d1ee8b3583a86d3057ddd895694e48
benqing.liu <benqing.liu@rea-group.com>
2018-01-14 20:59:03 +0800

    Change avatar className.

--- src/components/avatar/Avatar.js
+++ src/components/avatar/Avatar.js
@@ -5,7 +5,7 @@ import './avatar.css'
 export default class Avatar extends Component {
     render() {
         return (
-            <Image class='avatar' src='resources/avatar.jpg' circle/>
+            <Image className='avatar' src='resources/avatar.jpg' circle/>
         )
     }
 }
